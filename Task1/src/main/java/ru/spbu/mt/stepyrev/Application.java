bfad6203e2b733091ec80813ea3cab2cf068c06e
Daniil Stepyrev <stepirev_d_f@ntik.ru>
2021-10-25 19:45:44 +0300

    added initial project

---
+++ Task1/src/main/java/ru/spbu/mt/stepyrev/Application.java
@@ -0,0 +1,9 @@
+package ru.spbu.mt.stepyrev;
+
+/** A class that realizes application. */
+public class Application {
+    /** A method that starts the application. */
+    public static void main(String[] args) {
+
+    }
+}
