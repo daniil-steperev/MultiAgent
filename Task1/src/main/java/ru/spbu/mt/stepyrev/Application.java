5801975ce66eb7e6a200e0cab8f9918f4d99d0e4
Daniil Stepyrev <stepirev_d_f@ntik.ru>
2021-11-02 11:28:33 +0300

    modified Application class

--- Task1/src/main/java/ru/spbu/mt/stepyrev/Application.java
+++ Task1/src/main/java/ru/spbu/mt/stepyrev/Application.java
@@ -1,9 +1,15 @@
 package ru.spbu.mt.stepyrev;
 
+import ru.spbu.mt.stepyrev.util.NumberReader;
+
+import java.util.List;
+
 /** A class that realizes application. */
 public class Application {
     /** A method that starts the application. */
     public static void main(String[] args) {
-
+        List<Double> parameters = NumberReader.readInitParameters();
+        AlgorithmController controller = new AlgorithmController();
+        controller.initAgents(parameters);
     }
 }
